f264d4a52b8bf69e31a352d87b3845a35f830fe7
babacar22138 <babacarsow2001@gmail.com>
2026-08-19 23:22:04 +0200

    Premier commit du site GreenPhone

---
+++ js/header.js
@@ -0,0 +1,12 @@
+const burgerBtn = document.getElementById('burger');
+const menu_nav = document.querySelector('.menu-nav');
+const overlay = document.querySelector('.overlay');
+
+console.log(burgerBtn, menu_nav, overlay);
+
+burgerBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    menu_nav.classList.toggle('visible');
+    burgerBtn.classList.toggle('burger-deplace');
+    overlay.classList.toggle('overlay-visible');
+});
